a44d0f4acc0b4002798186a93c752d5b077c636c
ephys <hello@guylian.me>
2020-01-27 20:51:39 +0100

    feat: add new special wolf skins

--- src/main/java/be/ephys/wltdo/WolfSkin.java
+++ src/main/java/be/ephys/wltdo/WolfSkin.java
@@ -4,60 +4,69 @@ import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 public final class WolfSkin {
   public static final String WOLF_SKIN_KEY = WltdoMod.MODID + ":skin";
 
-  public static final String[] textureNames = new String[] {
-    "vanilla",
-    "black",
-    "collie1",
-    "collie2",
-    "dalmatian",
-    "dog1",
-    "huskylike",
-    "white",
-    "moonmoon",
-//    "zombie"
-    //    "fire",
-    //    "skelly",
-    //    "creeper",
-  };
+  private static final Map<Integer, String> textures = new HashMap<>();
 
-  private static final ResourceLocation[][] textureCache = new ResourceLocation[WolfSkin.textureNames.length][];
+  private static final int[] regular = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+  private static final int[] moonmoons = new int[] { 8, 9 };
+
+  static {
+    textures.put(0, "vanilla");
+    textures.put(1, "black");
+    textures.put(2, "collie1");
+    textures.put(3, "collie2");
+    textures.put(4, "dalmatian");
+    textures.put(5, "dog1");
+    textures.put(6, "huskylike");
+    textures.put(7, "white");
+
+    // rare spawn
+    textures.put(8, "moonmoon");
+    textures.put(9, "huskymoonmoon");
+
+    // special spawn
+    textures.put(10, "ice");
+    textures.put(11, "fire");
+
+    textures.put(12, "halloween");
+    textures.put(13, "rainbow");
+
+    textures.put(16, "creeper");
+
+    // TODO necrodermal virus
+    textures.put(14, "zombie");
+    // TODO Nullodermal Virus
+    textures.put(15, "skelly");
+  }
+
+  private static final Map<Integer, ResourceLocation[]> textureCache = new HashMap<>();
 
   static {
     // vanilla textures
-    textureCache[0] = new ResourceLocation[] {
+    textureCache.put(0, new ResourceLocation[] {
       new ResourceLocation("textures/entity/wolf/wolf.png"),
       new ResourceLocation("textures/entity/wolf/wolf_tame.png"),
       new ResourceLocation("textures/entity/wolf/wolf_angry.png")
-    };
-
-    for (int i = 1; i < textureNames.length; i++) {
-      getTextures(i);
-    }
-  }
-
-  public static int availableTextureCount() {
-    return textureNames.length;
+    });
   }
 
   public static ResourceLocation[] getTextures(int id) {
-    if (textureCache[id] != null) {
-      return textureCache[id];
+    if (textureCache.containsKey(id)) {
+      return textureCache.get(id);
     }
 
-    String textureName = textureNames[id];
+    String textureName = textures.get(id);
     ResourceLocation[] textures = new ResourceLocation[] {
       new ResourceLocation(WltdoMod.MODID, "textures/entity/wolf/" + textureName + "/neutral.png"),
       new ResourceLocation(WltdoMod.MODID, "textures/entity/wolf/" + textureName + "/tame.png"),
       new ResourceLocation(WltdoMod.MODID, "textures/entity/wolf/" + textureName + "/angry.png")
     };
 
-    textureCache[id] = textures;
+    textureCache.put(id, textures);
 
     return textures;
   }
@@ -68,15 +77,13 @@ public final class WolfSkin {
   }
 
   public static int getWolfSkinId(UUID uuid) {
-    int textureCount = availableTextureCount();
-
     Random rand = new Random(uuid.getLeastSignificantBits() ^ uuid.getMostSignificantBits());
 
     if (rand.nextFloat() < 0.03) {
-      return 8; // MOON MOON
+      return moonmoons[rand.nextInt(moonmoons.length - 1)];
     }
 
-    return rand.nextInt(textureCount - 1);
+    return regular[rand.nextInt(regular.length - 1)];
   }
 
   public static int getWolfSkinId(EntityWolf wolf) {
